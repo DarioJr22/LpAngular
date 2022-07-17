c24b09f03fae7e1fc8b6ef609b19488a9471978c
DarioJr22 <dariojr1233@gmail.com>
2022-07-16 21:32:50 -0300

    Primeiro Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'LpAngular';
+
+  test = [1,2,3,4]
+}
